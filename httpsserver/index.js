39c6c037a86be8a4cb0685d5f09e3cdd4512757a
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-11-05 16:30:47 +0530

    Add files via upload

--- httpsserver/index.js
+++ httpsserver/index.js
@@ -14,13 +14,39 @@ eg url,request header , and data.
 */
 
 
+/*
 const http = require('http');
 
 const server = http.createServer((req, res)  => {
     res.end("Hello from other side!");
 });
 
-server.listen(3000 ," 127.0. 0.1."  , () => {
+server.listen(49153 ," 127.0.0.1."  , () => {
 
-    console.log("listening to the port no 8000");
+    console.log("listening to the port no 49153");
 });
+
+*/
+
+
+/*const http = require("http");
+
+http
+  .createServer((req, resp) => {
+    resp.write("Hello from server side");
+    resp.end();
+  })
+  .listen(4500);
+  */
+  const http = require("http");
+
+  const server = http.createServer((req, res) => {
+    res.end("Hello from other side ");
+  });
+  
+  server.listen(49154, "127.0.0.1", () => {
+    console.log("listening to the port no 49154");
+  }); 
+
+
+
